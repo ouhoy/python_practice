f372a66ec40ab7fad833d493a17837c0f3d04df6
ouhoy <kimkinon@gmail.com>
2022-11-01 23:01:25 +0100

    Improve Code Quality

--- Password Generator/main.py
+++ Password Generator/main.py
@@ -9,33 +9,25 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-generated_letters = ""
-genrated_numbers = ""
-genrated_symbols = ""
+
 generated_password = []
 for i in range(0, nr_letters):
-    random_letter =  random.randint(0, len(letters) - 1)
-    generated_letters  += letters[random_letter]
-    generated_password.append(letters[random_letter])
+    random_letter =  letters[random.randint(0, len(letters) - 1)]
+    generated_password.append(random_letter)
 
 for n in range(0, nr_numbers):
-    random_number =  random.randint(0, len(numbers) - 1)
-    genrated_numbers  += numbers[random_number]
-    generated_password.append(numbers[random_number])
+    random_number =  numbers[random.randint(0, len(numbers) - 1)]
+    generated_password.append(random_number)
 
 for l in range(0, nr_symbols):
-    random_symbol =  random.randint(0, len(symbols) - 1)
-    genrated_symbols  += symbols[random_symbol]
-    generated_password.append(symbols[random_symbol])
+    random_symbol =  symbols[random.randint(0, len(symbols) - 1)]
+    generated_password.append(random_symbol)
 
-edited_password = generated_password
 final_password = ""
-num_count = 0
 for m in range(0,len(generated_password)):
-    num_count+= 1
-    random_num = random.randint(0,  len(edited_password) - 1)
-    final_password += edited_password[random_num]
-    edited_password.pop(random_num)
+    random_num = random.randint(0,  len(generated_password) - 1)
+    final_password += generated_password[random_num]
+    generated_password.pop(random_num)
 
 
 print(final_password)    
