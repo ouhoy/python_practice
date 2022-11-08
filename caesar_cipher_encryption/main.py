e0d4e42d00a81f6f69afcb729e4bcb9dfedd0eca
ouhoy <kimkinon@gmail.com>
2022-11-08 17:59:54 +0100

    Add Encryption Part
    --Not fully tested
    --I have to finish the Decryption part :smile:

--- caesar_cipher_encryption/main.py
+++ caesar_cipher_encryption/main.py
@@ -3,3 +3,24 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+encrypted = []
+decrypted = []
+
+def encryption(_text, _shift):
+    for letter in _text:
+        shift_number = alphabet.index(letter) + _shift
+        if shift_number > len(alphabet) - 1:
+            encrypted.append(alphabet[+ _shift -(len(alphabet) + 1)])
+        else:
+            encrypted.append(alphabet[shift_number])
+        
+    print("".join(encrypted))
+def decryption(_text, _shift):
+    print()
+
+
+if direction.strip() == "encode":
+    print("encoding...")
+    encryption(text, shift)
+if direction.strip() == "decode":
+    decryption(text, shift)   
