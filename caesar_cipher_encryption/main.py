26ce6f3757cabbebe8de99417e2106e81c162bad
ouhoy <kimkinon@gmail.com>
2022-11-08 20:58:01 +0100

    Improve the UX of the program

--- caesar_cipher_encryption/main.py
+++ caesar_cipher_encryption/main.py
@@ -1,16 +1,30 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from assets import logo, alphabet
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").strip()
-text = input("Type your message:\n").lower().strip()
-shift = int(input("Type the shift number:\n"))
+print(logo)
+
 final_text = []
+restart_cipher = True
 
-def ceasar(_text, _shift, operator):
-  
-    if operator == "decode":
-        _shift *= -1
-    for letter in _text:
-        shift_number = alphabet.index(letter) + _shift
-        final_text.append(alphabet[shift_number])
-    print(f"Your {operator}d text is : ","".join(final_text)) 
-ceasar(text, shift, direction)
+
+while restart_cipher:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").strip()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n")) % 26
+    def ceasar(_text, _shift, operator):
+        if operator == "decode":
+            _shift *= -1
+        for letter in _text:
+            if letter in alphabet:
+                shift_number = alphabet.index(letter) + _shift
+                final_text.append(alphabet[shift_number])
+            else:
+                final_text.append(letter)    
+        print(f"Your {operator}d text is : ","".join(final_text)) 
+    ceasar(text, shift, direction)
+    final_text = []
+    restart = input("Would you like to restart cipher?\nType 'yes' if you want to go again. Otherwise type 'no'. ").lower().strip()
+    if restart == "no":
+        restart_cipher = False
+        print("Goodbye")
+
+        
