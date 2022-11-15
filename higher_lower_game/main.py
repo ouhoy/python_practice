480affe512dec669cc58aafa5fcf711f4028942c
ouhoy <kimkinon@gmail.com>
2022-11-15 21:29:27 +0100

    Add little improvement

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -3,7 +3,6 @@ import random
 from game_data import data
 from art import logo, vs
 
-# data.index(random_choice_a)
 def random_account(account):
     return random.choice(account)
 
@@ -17,24 +16,28 @@ def get_data(data):
     return f"{name}, a {description}, from {country}"
 
 def compare_followers(guess, a ,b):
-    if guess == "a" and a >= b or guess == "b" and b >= a: return True 
+    global account_a, account_b
+    if guess == "a" and a >= b or guess == "b" and b >= a: 
+        if a >= b:
+            account_b =  random_account(data)
+        else:
+            account_a =  random_account(data)    
+        return True 
     else: return False
 
 has_lost = False  
+print(logo)
 while not has_lost:
-    print(logo)
     print("Compare A: ",get_data(account_a))
     print(vs, f"\n Against B: {get_data(account_b)}")
 
     comparison = input("Type 'A' or 'B' ").lower().strip()
+    if comparison != "a" and comparison != "b":
+        continue
     if compare_followers(comparison, account_a["follower_count"], account_b["follower_count"]):
         score += 1
-        if account_a["follower_count"] >= account_b["follower_count"]:
-            account_b =  random_account(data)
-        else:
-            account_a =  random_account(data)    
         print(f"Your score is {score}") 
-    
+
     else:
-        print("You have lost")   
+        print(f'''You have lost {account_a["name"]} has {account_a["follower_count"]} followers and {account_b["name"]} has {account_b["follower_count"]} followers''')   
         has_lost = True 
