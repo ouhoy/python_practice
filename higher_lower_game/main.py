12dc5b3e3b6c02b8c74385f98f373cb854141c76
ouhoy <kimkinon@gmail.com>
2022-11-15 22:44:54 +0100

    Finish the code
    Added some validations
    Made the code more clean and readable

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -1,8 +1,10 @@
 import random
-
+import os
 from game_data import data
 from art import logo, vs
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 def random_account(account):
     return random.choice(account)
 
@@ -16,28 +18,35 @@ def get_data(data):
     return f"{name}, a {description}, from {country}"
 
 def compare_followers(guess, a ,b):
-    global account_a, account_b
-    if guess == "a" and a >= b or guess == "b" and b >= a: 
-        if a >= b:
-            account_b =  random_account(data)
-        else:
-            account_a =  random_account(data)    
-        return True 
-    else: return False
+    if a > b:
+        return guess == "a"
+    else:
+        return guess == "b"    
 
 has_lost = False  
-print(logo)
+
 while not has_lost:
+    cls()
+    print(logo)
+    if score > 0: print(f"You are right! Current score: {score}")
+    if account_a == account_b:
+       account_b: random_account(data)
     print("Compare A: ",get_data(account_a))
     print(vs, f"\n Against B: {get_data(account_b)}")
+    comparison = input("Who has more followers? Type 'A' or 'B': ").lower().strip()
 
-    comparison = input("Type 'A' or 'B' ").lower().strip()
     if comparison != "a" and comparison != "b":
         continue
-    if compare_followers(comparison, account_a["follower_count"], account_b["follower_count"]):
+    is_right_answer = compare_followers(comparison, account_a["follower_count"], account_b["follower_count"])
+    if is_right_answer:
+        if account_a["follower_count"] >= account_b["follower_count"]: 
+            account_b =  random_account(data)
+        else: 
+            account_a =  account_b
+            account_b = random_account(data)
         score += 1
-        print(f"Your score is {score}") 
-
+        
     else:
-        print(f'''You have lost {account_a["name"]} has {account_a["follower_count"]} followers and {account_b["name"]} has {account_b["follower_count"]} followers''')   
+        print(f"Sorry that's worng. Final score {score}")
+        print(f'''{account_a["name"]} has {account_a["follower_count"]}M followers while {account_b["name"]} has {account_b["follower_count"]}M followers.''')   
         has_lost = True 
