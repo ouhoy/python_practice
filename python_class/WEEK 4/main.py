f79deb47372df972743cf8b5d8d197fe7d994f0f
ouhoy <kimkinon@gmail.com>
2022-11-17 10:39:58 +0100

    Add Py class WEEK3

---
+++ python_class/WEEK 4/main.py
@@ -0,0 +1,17 @@
+x = 20
+if x < 2 :
+    print('Small')
+elif x < 10:
+    print("Medium")
+print('All done')
+
+rawstr = input("Enter a number: ")
+try:
+    ival = int(rawstr)
+except:
+    ival = -1
+
+if ival > 0:
+    print("Nice work")
+else:
+    print("Not a number")    
