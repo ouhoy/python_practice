efb5bd20713f0196f0a05d1f36dea7709ec34e1d
ouhoy <kimkinon@gmail.com>
2022-11-19 16:04:58 +0100

    OOP with the Coffee Machine

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,13 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# TODO: 1. Print Report
+machine = CoffeeMaker()
+print(machine.report())
+# TODO: 2. Check Resources Sufficient?
+print(machine.is_resource_sufficient())
+# TODO: 3. Process Coins
+# TODO: 4. Check Transaction Successful
+# TODO: 5. Make Coffee
+
