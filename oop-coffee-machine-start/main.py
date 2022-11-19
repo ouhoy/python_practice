21556a7cb07a444eef328625a3dbba1841cb2157
ouhoy <kimkinon@gmail.com>
2022-11-19 16:43:49 +0100

    Finish the OOP Coffee Machine Project

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,12 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# TODO: 1. Print Report
-machine = CoffeeMaker()
-print(machine.report())
-# TODO: 2. Check Resources Sufficient?
-print(machine.is_resource_sufficient())
-# TODO: 3. Process Coins
-# TODO: 4. Check Transaction Successful
-# TODO: 5. Make Coffee
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower().strip()
+    if choice == "off":
+        is_on = False
+        break
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
