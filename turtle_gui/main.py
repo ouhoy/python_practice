935a843eb5cef764daa343009ef9395d35eb7943
ouhoy <kimkinon@gmail.com>
2022-11-24 22:30:44 +0100

    Drawing Different Shapes

--- turtle_gui/main.py
+++ turtle_gui/main.py
@@ -2,15 +2,36 @@ from turtle import Turtle
 
 tim = Turtle()
 
-# Draw a Square
-for _ in range(4):
-    tim.forward(100)
-    tim.left(90)
 
-# Draw a Dashed Line
-for _ in range(15):
-    tim.forward(10)
-    tim.color("white", "black")
-    tim.forward(10)
-    tim.color("black")
+def dashed_line():
+    for _ in range(15):
+        tim.forward(10)
+        tim.penup()
+        tim.forward(10)
+        tim.pendown()
+
+
+def square():
+    for _ in range(4):
+        tim.right(90)
+        tim.forward(100)
+
+
+def pentagon(sides):
+    deg = 360/sides
+    for _ in range(sides):
+        tim.right(deg)
+        tim.forward(100)
+
+
+def triangle():
+    for _ in range(3):
+        tim.right(120)
+        tim.forward(100)
+
+
+triangle()
+square()
+for num in range(5, 9):
+    pentagon(num)
 
