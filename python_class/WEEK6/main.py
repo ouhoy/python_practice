208d8f2788e611a5799e1b564f649d1bd1c33180
ouhoy <kimkinon@gmail.com>
2022-12-06 17:30:20 +0100

    End of Py Class

---
+++ python_class/WEEK6/main.py
@@ -0,0 +1,25 @@
+while True:
+    line = input("> ").strip().lower()
+    if line[0] == "#":
+        continue
+    if line == "done":
+        break
+    print(line)
+
+print("Done!")
+
+num_list = [3, 41, 12, 9, 74, 15]
+smallest_num = num_list[0]
+
+for num in num_list:
+
+    if num < smallest_num:
+        smallest_num = num
+
+print(f"The smallest number is {smallest_num}")
+
+for i in range(len(num_list)):
+
+    if num_list[i] < smallest_num:
+        smallest_num = num_list[i]
+print(f"The smallest number in the second method is {smallest_num}")
