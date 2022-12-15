2f12c4611063cde4bcc0f210f971123b45b21944
ouhoy <kimkinon@gmail.com>
2022-12-15 11:28:02 +0100

    Reading TXT files with python

--- python_class/WEEK7/main.py
+++ python_class/WEEK7/main.py
@@ -6,3 +6,12 @@ if "a" in fruit:
     print("Found")
 
 print(counter)
+
+stuff = "Hello, world!!"
+
+my_file = open("./my_text.txt", "r")
+inp = my_file.read()
+
+for li in inp:
+    print(li)
+print("read function: ", len(inp))
