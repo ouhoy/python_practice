2b929d1a7ed160920a34f358aff4d510fe077156
ouhoy <kimkinon@gmail.com>
2023-02-15 21:22:49 +0100

    Detect collision with wall or tail

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -33,11 +33,19 @@ while game_is_on:
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     # Detect collision with wall.
     if snake.collision_with_wall():
         scoreboard.game_over()
         game_is_on = False
+    # Detect collision with tale.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- snakeGame/snake.py
+++ snakeGame/snake.py
@@ -18,11 +18,17 @@ class Snake:
 
     def initiate_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
