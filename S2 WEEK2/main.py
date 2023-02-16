c00e312e7bb974190be300d26e1f7e9409c4b3b4
ouhoy <kimkinon@gmail.com>
2023-02-16 12:47:54 +0100

    Convert data type in the circle class

--- S2 WEEK2/main.py
+++ S2 WEEK2/main.py
@@ -6,11 +6,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Circle")
 
-red_circle = Circle(20, [0, 0], "red")
+red_circle = Circle(20, (0, 0), "red")
 
-red_circle.move([200, 20])
+red_circle.move((200, 20))
 red_circle.draw(29)
 
-blue_circle = Circle(10, [0, 0], "blue")
+blue_circle = Circle(10, (0, 0), "blue")
 
 screen.exitonclick()
