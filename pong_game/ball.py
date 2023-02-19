dc0cec70f175376c00c2ffd0317a4aee15d2f158
ouhoy <kimkinon@gmail.com>
2023-02-19 18:29:38 +0100

    Speed the ball in each bounce

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -15,18 +15,21 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
 
-    def bounce_y(self):
-        self.y_move *= -1
-
-    def bounce_x(self):
-        self.x_move *= -1
-
     def move(self):
         new_xcor = self.xcor() + self.x_move
         new_ycor = self.ycor() + self.y_move
 
         self.goto(x=new_xcor, y=new_ycor)
 
+    def bounce_y(self):
+        self.y_move *= -1
+        self.move_speed *= 0.9
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
     def reset_position(self):
         self.goto(0, 0)
         self.bounce_x()
+        self.move_speed = 0.1
--- pong_game/main.py
+++ pong_game/main.py
@@ -25,7 +25,7 @@ screen.onkeypress(left_paddle.down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
