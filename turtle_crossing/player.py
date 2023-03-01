719d739453ab0d2a68e3d32072070d2b94f30a1c
ouhoy <kimkinon@gmail.com>
2023-03-01 13:59:05 +0100

    Create player

--- turtle_crossing/player.py
+++ turtle_crossing/player.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.right(-90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+        # When the turtle hits the top edge of the screen
+        y_position = self.ycor()
+        if y_position > 280:
+            self.goto(STARTING_POSITION)
