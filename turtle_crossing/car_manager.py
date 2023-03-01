960691044ab6506e98c521bc9727b90df987f994
ouhoy <kimkinon@gmail.com>
2023-03-01 14:33:18 +0100

    create first car

--- turtle_crossing/car_manager.py
+++ turtle_crossing/car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.create_car()
+
+    def create_car(self):
+        self.penup()
+        self.shape("square")
+        self.shapesize(20, 40)
+        self.goto(0, 0)
+
+    def move(self):
+        self.forward(MOVE_INCREMENT)
