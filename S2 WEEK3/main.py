64c24212a61b7219bc6630a10f23f3c5ecafb326
ouhoy <kimkinon@gmail.com>
2023-03-02 10:45:35 +0100

    Add school class and description

--- S2 WEEK3/main.py
+++ S2 WEEK3/main.py
@@ -2,6 +2,7 @@ from student import Student
 from lecturer import Lecturer
 from program import Program
 from course import Course
+from school import School
 
 student_one = Student("Abdallah", "Dahmou", 23)
 student_two = Student("Rania", "Samih", 22)
@@ -14,9 +15,15 @@ lecturer_one = Lecturer("Younes", "El Amrani", [quality_software])
 SE = Program("Software Engineering", [quality_software, web_development], [student_one])
 BA = Program("Business Management", [quality_software], [student_two])
 
-# List enrolled software engineering students
-for num, student in enumerate(SE.get_students()):
-    first_name = student.get_fullname()["firstname"]
-    last_name = student.get_fullname()["lastname"]
+SIST = School("SIST", "School of software engineering and business", [SE, BA])
 
-    print(f"{num + 1} - {first_name} {last_name}")
+
+# List enrolled students
+def enrolled_students(program):
+    for num, student in enumerate(program):
+        first_name = student.get_fullname()["firstname"]
+        last_name = student.get_fullname()["lastname"]
+
+        print(f"{num + 1} - {first_name} {last_name}")
+
+# enrolled_students(SE.get_students())
--- S2 WEEK3/school.py
+++ S2 WEEK3/school.py
@@ -1,7 +1,8 @@
 class School:
-    def __init__(self, name: str, programs: list):
+    def __init__(self, name: str, description: str, programs: list):
         self.__name = name
+        self.__description = description
         self.__programs = programs
 
     def display(self):
-        return {"name": self.__name, "programs": self.__programs}
+        return {"name": self.__name, "description": self.__description, "programs": self.__programs}
